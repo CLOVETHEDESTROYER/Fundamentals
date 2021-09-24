5788dbe433f225e3c7754bd9e960e0361e112552
Carlos Alvarez <thesocialiq@gmail.com>
2021-09-23 20:39:24 -0600

    added variables-and-data.js added objects-apis.js added conditionals-and-functions.js

---
+++ variables-and-data.js
@@ -0,0 +1,35 @@
+/*
+ * Variables, Data Types, amd Typing
+ */
+let wordData = "Word list";
+
+console.log(wordData);
+
+let subtotal = 100;
+let tax = subtotal * 0.07
+let totalWithTax = tax + subtotal;
+
+let obj = {
+    key1: "value",
+    key2: 4,
+    boolean: true,
+    obj2: {
+        obj2key1: "Internal Object Value",
+    }
+};
+
+console.log(obj.obj2Key1);
+
+let variable : string = "string";
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2
+
+console.log(result);
+
+Let add = 1 +2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4/ 2;
+let mod = 5 % 2;
